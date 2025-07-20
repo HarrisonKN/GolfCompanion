11ae6f495b55c03f914873c89ee673359733e61a
Harrison <Hazabone2233@gmail.com>
2025-07-20 21:06:39 +1000

    fuck you theme.ts

--- golf-companion/constants/theme.ts
+++ golf-companion/constants/theme.ts
@@ -1,4 +1,16 @@
 // constants/theme.ts
+export const COLORS = {
+  primary: "#609966",
+  background: "#E9EBF8", //Lavender(Web) think this is a really nice off white color not as harsh 
+  secondary: "#9DC08b",
+  third:"#4B5842",
+  error: "#EF4444",
+  textDark: "#111827",
+  textLight: "#6B7280",
+  white: "#FFFFFF",
+  black: "#000000",
+  grey: "#9CA3AF",
+};
 
 export const SIZES = {
   base: 8,
